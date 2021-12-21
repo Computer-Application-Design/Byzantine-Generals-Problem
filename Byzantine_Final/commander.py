3f5036e6a31fb98a6a0cc0daea5e0050e22a11f7
Hyunseok Ko <kohyunsuk98@gmail.com>
2021-12-21 18:51:19 +0900

    일체형

--- Byzantine_Final/commander.py
+++ Byzantine_Final/commander.py
@@ -20,32 +20,58 @@ def main():
         log_message = "{}:{}\n".format("Commander","김태용")
         file_object.write(log_message)
             
-
-
-
     time.sleep(5)
     filename2 = "".join([log_file_dir_path, "result",".txt"])
     fault = 0 # Fault메시지 갯수 세기 위해서
     number = 0 # General 갯수
     f = open(filename2,"rt",encoding='utf-8')
+
+    print("Case1) node: 3ea, byzantine: 1ea")
+
     while True:
         lst = f.readline().split(':')
-        if(lst[1]=="홍길동\n"):
-            fault = fault + 1
-            number = number + 1
-            log_message = "<{} : {}>\n".format(lst[0],'홍길동')
-            print(log_message)
-        else:
-            number = number + 1
-            log_message = "<{} : {}>\n".format(lst[0],'김태용')
-            print(log_message)
-        if(number == 4):
+        if(lst[0]!="General4"):
+            if(lst[1]=="홍길동\n"):
+                fault = fault + 1
+                number = number + 1
+                log_message = "<{} : {}>".format(lst[0],'홍길동')
+                print(log_message)
+            else:
+                number = number + 1
+                log_message = "<{} : {}>".format(lst[0],'고현석')
+                print(log_message)
+        if(number == 3):
             break
     
     if(number < (3*fault) + 1):
-        print("합의 불가능")
+        print("\n-> 합의 불가능\n")
     else:
-        print("합의 가능")
+        print("\n-> 합의 가능\n")
+
+    print("Case2) node: 4ea, byzantine: 1ea")
+    print("-> 추가 General만 출력\n")
+
+    n = 0
+
+    while True:
+        lst = f.readline().split(':')
+        if(lst[0]=="General4"):
+            if(lst[1]=="홍길동\n"):
+                fault = fault + 1
+                log_message = "<{} : {}>".format(lst[0],'홍길동')
+                print(log_message)
+            else:
+                number += 1
+                log_message = "<{} : {}>".format(lst[0],'고현석')
+                print(log_message)
+            n += 1
+        if(n==1):
+            break
+    
+    if(number < (3*fault) + 1):
+        print("\n-> 합의 불가능")
+    else:
+        print("\n-> 합의 가능")
         
     f.close()
 
